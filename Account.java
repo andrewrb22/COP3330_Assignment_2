939c0e444816b5d86375f90b95f5daa76c08c603
Andrew Rodriguez <andrewrb22@gmail.com>
2022-09-12 20:31:25 -0400

    Assigment 2 - object - Created account class and bank class. Account class ready, bank class just the begining

--- Account.java
+++ Account.java
@@ -1,35 +1,104 @@
+import java.util.Scanner;
+
 /**
  * Account
  */
 
-
- /*
-  Within the Bank Class do the following:
-Create a private static array of Account.   The size of the array will be 5.
-Within the main method, create a menu that asks:
-Displays the name of each account and the number.
-Which account would you like to work on (Assign a value 1-5 to each account)
-Exit Program
-When they select the account, they can do the following:
-If empty, it automatically creates an account.
-To create an account, you need to ask the following information:
-Account name
-Interest rate (formatted as a double for example: 1.5)
-If it is not empty ask the user if they would like to do the following:
-Deposit Funds
-Withdraw Funds
-Check Balance
-Calculate Interest
-Delete Account
-For each of these but delete will call a method in the Account class.
-  You will need to ask for the information here.
-Delete account sets the index value to Null. 
-Deposit, and Withdraw will return true if the operation is successful 
-and a false if you are unable to complete it.  Do not check to see
- the user entered valid options here, allow the Account class to handle it. * 
-
-  */
+/*
+ * Within the Bank Class do the following:
+ * Create a private static array of Account. The size of the array will be 5.
+ * Within the main method, create a menu that asks:
+ * Displays the name of each account and the number.
+ * Which account would you like to work on (Assign a value 1-5 to each account)
+ * Exit Program
+ * When they select the account, they can do the following:
+ * If empty, it automatically creates an account.
+ * To create an account, you need to ask the following information:
+ * Account name
+ * Interest rate (formatted as a double for example: 1.5)
+ * If it is not empty ask the user if they would like to do the following:
+ * Deposit Funds
+ * Withdraw Funds
+ * Check Balance
+ * Calculate Interest
+ * Delete Account
+ * For each of these but delete will call a method in the Account class.
+ * You will need to ask for the information here.
+ * Delete account sets the index value to Null.
+ * Deposit, and Withdraw will return true if the operation is successful
+ * and a false if you are unable to complete it. Do not check to see
+ * the user entered valid options here, allow the Account class to handle it. *
+ * 
+ */
 public class Account {
 
-    
+  /******************************
+   * Variables
+   *********************************/
+  private String name;
+  private double rate;
+  private double balance;
+
+  /******************************
+   * Constructure
+   *********************************/
+  public Account(String name, double rate, double balance) {
+
+    this.name = name;
+    this.rate = rate;
+    this.balance = balance;
+
+  }
+
+  /******************************
+   * METHODS
+   *********************************/
+
+  public double deposit(double d) {
+
+    if (d > 0.0) {
+
+      this.balance = (this.balance + d);
+
+    } else
+      d = 0.0;
+
+    return this.balance;
+
+  }
+
+  public double withdraw(double w) {
+
+    if (w <= 0.0 || w > balance) {
+      w = 0.0;
+    } else
+      this.balance = this.balance - w;
+
+    return this.balance;
+  }
+
+  public double getBalance() {
+
+    return this.balance;
+  }
+
+  public String getName() {
+
+    return this.name;
+  }
+
+  public double applyRate(int m, double rate) {
+
+    if (m > 0 && m < 1000) {
+
+      m = 10 * (m / 12) / 100;
+
+
+    } else
+      rate = 0.0;
+
+    return rate =  100 * (1 + m);
+
+  }
+
 }
--- Bank.java
+++ Bank.java
@@ -1,32 +1,40 @@
+import java.util.Scanner;
+
 /**
  * Bank
  */
 public class Bank {
 
-  //Intance Variable
-  private String name;
-  private double balance;
-  private double interestRate;
-  
-  //Constructure to take account name and rate
-    public Bank(String n , double r){
+public static void main(String[] args) {
+     int choice = 0;
+    
+     Account[]bank = new Account[5];
 
-        this.name = n;
-        this.interestRate = r;
-    }
 
-    /*
-     * Create the following public instance methods:
-deposit - takes in a double that will represent the funds that should be deposited.  If the number is 0 or less, you cannot deposit this and return false, otherwise it will add this amount to the balance and return true.   
-withdraw - takes in a double that will represent the funds that should be taken from the balance.  If the number is 0 or less or would make the balance less than 0 then you cannot withdraw this amount of funds and return false.  Otherwise, update the balance and return true.
-getBalance - returns the balance as a double
-getName - returns the name of the account
-calcInterest - takes in an integer that will have value from 1-1,000 that represents how many months have passed.  Calculate interest that should be added to the account.  Example: If the balance is $100, interest is 10% and the parameter's value is 6 (months), then the new balance would be $105.00. 
 
-Math:
+bank[2] = new Account("Andrew", 6.5, 10.0) ;
 
-InterestPeriod = 10*(6/12)/100
-newTotal = 100 * (1 + InterestPeriod)
-     */
+
+     System.out.println("Account Name: " + bank[2].getName() + "  Wiht a Balance of:" + bank[2].getBalance());
+
+   choice = 6;
+   double tax = 0.0;
+
+ System.out.println( "New Balance: " + (bank[2].getBalance() + bank[2].applyRate(choice , tax)));
+
+     
+     // while (choice != 6) {
+        
+          
+
+
+
+
+
+
+     // }
+
+}
+
 
 }
